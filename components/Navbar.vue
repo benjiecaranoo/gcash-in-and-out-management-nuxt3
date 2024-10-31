dad7221dcd8c7e6e66de6131e87b13b66b8a7f3d
Benjie Caranoo <caranoobenjie@gmail.com>
2024-10-31 15:16:59 +0800

    [NavBar] - fixed issue with hide and show  navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { useRouter } from 'vue-router'
 import { useAuthStore } from '~/stores/auth'
 import { HomeIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
@@ -13,7 +13,7 @@ const isDrawerOpen = ref(false)
 const router = useRouter()
 const toggleDrawer = () => isDrawerOpen.value = !isDrawerOpen.value
 
-isDrawerOpen.value = authStore.isAuthenticated
+const isAuthenticated = computed(() => authStore.isAuthenticated)
 
 const authRoutes = [
   { name: 'Dashboard', href: '/', icon: HomeIcon },
@@ -32,47 +32,46 @@ const userLogout = async () => {
 </script>
 <template>
   <div>
-    <v-app-bar flat elevation="0" v-if="isAuthenticated" color="primary" class="">
+    <v-app-bar flat elevation="0" v-if="isAuthenticated" color="primary">
       <v-app-bar-nav-icon @click="toggleDrawer" v-if="$vuetify.display.smAndDown" />
-      <!-- <v-app-bar-nav-icon>
-        <v-img src="../logo.png" width="48px" height="48px" class="mb-12" /> -->
       <v-toolbar-title class="text-center">Gcash In and Out Tracker</v-toolbar-title>
     </v-app-bar>
-      <v-navigation-drawer
-        v-if="isAuthenticated"
-        v-model="isDrawerOpen"
-        :permanent="$vuetify.display.mdAndUp"
-        :temporary="$vuetify.display.smAndDown"
-        color="primary"
-      >
-        <v-list>
-          <v-list-item
-            prepend-avatar="https://randomuser.me/api/portraits/men/78.jpg"
-            :title="authStore.user?.name || 'User'"
-            :subtitle="authStore.user?.email || 'user@example.com'"
-          />
-        </v-list>
 
-        <v-divider />
-        <v-list nav>
-          <v-list-item 
-            v-for="route in authRoutes" 
-            :key="route.name" 
-            :prepend-icon="route.icon" 
-            :title="route.name" 
-            :to="route.href" 
-          />
-        </v-list>
-        <v-spacer/>
+    <v-navigation-drawer
+      v-if="isAuthenticated"
+      v-model="isDrawerOpen"
+      :permanent="$vuetify.display.mdAndUp"
+      :temporary="$vuetify.display.smAndDown"
+      color="primary"
+    >
+      <v-list>
+        <v-list-item
+          prepend-avatar="https://randomuser.me/api/portraits/men/78.jpg"
+          :title="authStore.user?.name || 'User'"
+          :subtitle="authStore.user?.email || 'user@example.com'"
+        />
+      </v-list>
 
-        <template v-slot:append>
-          <div class="pa-2">
-            <v-btn block prepend-icon="mdi-logout" flat @click="userLogout()">
-              Logout
-            </v-btn>
-          </div>
-        </template>
-      </v-navigation-drawer>
+      <v-divider />
+      <v-list nav>
+        <v-list-item 
+          v-for="route in authRoutes" 
+          :key="route.name" 
+          :prepend-icon="route.icon" 
+          :title="route.name" 
+          :to="route.href" 
+        />
+      </v-list>
+      <v-spacer/>
+
+      <template v-slot:append>
+        <div class="pa-2">
+          <v-btn block prepend-icon="mdi-logout" flat @click="userLogout()">
+            Logout
+          </v-btn>
+        </div>
+      </template>
+    </v-navigation-drawer>
   </div>
 </template>
 <style lang="scss" scoped>
